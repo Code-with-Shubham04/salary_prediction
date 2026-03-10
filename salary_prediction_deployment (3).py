b5e189c9938592016c244e74acfbb65daeab190b
Shubham Tambe <subhashtambe0012@gmail.com>
2026-03-10 15:06:00 +0530

    Fix DataFrame column names for salary prediction

--- salary_prediction_deployment (3).py
+++ salary_prediction_deployment (3).py
@@ -23,11 +23,11 @@ job_title = st.selectbox("Select your job title", encoder["Job Title"].classes_)
 experience = st.number_input("Enter your experience (in years)",0,50)
 
 df = pd.DataFrame({
-    "Enter your age":[age],
-    "Select your gender":[gender],
-    "Select your education":[education],
-    "Select your education":[job_title],
-    "Enter your experience (in years)":[experience]
+    "Age":[age],
+    "Gender":[gender],
+    "Education Level":[education],
+    "Job Title":[job_title],
+    "Experience":[experience]
 
 })
 
